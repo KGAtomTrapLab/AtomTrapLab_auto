e9f3eba1e1b1268e9842ee8b4c526d9475e82d2a
jchuri <38012139+jchuri@users.noreply.github.com>
2020-01-29 19:49:02 -0800

    Updated LabVIEW run and set laser values

--- MOT.py
+++ MOT.py
@@ -1,8 +1,9 @@
-from Laser_controler_functions import *
+import Laser_controler_functions
 import matplotlib
 import sys
+import os
 
-'''
+"""
 This program automatically tunes the pump and trap lasers from the lab
 inputs: 
     - Laser Current [mA]
@@ -24,35 +25,46 @@ Outputs:
         - Fabrey Perro 
         - Error
     - Transition match
-'''
+"""
 
 
 def find_threshold_current():
     pass
 
-def set_laser_values(current, temp, laserControler):
+
+def set_laser_values(current, temp, laserController):
+    Laser_controller_functions.setCurrent(current, laserController)
+    Laser_controller_functions.setTempurature(temp, laserController)
+
+
+def run_labVIEW(Vmax, Vmin, sweepTime):
+    os.system(
+        f"C:\\Users\\Kat\\Desktop\\builds\\Application.exe -- {Vmax} {Vmin} {sweepTime}"
+    )  # Need to find actuall location
+
+
+def read_csv():  # Anya's Code
     pass
 
-def run_labVIEW(Vmax,Vmin,sweepTime):
+
+def plot_graphs():  # Inputs from csv
     pass
 
-def read_csv(): # Anya's Code
+
+def cursors_inputs():  # Cursers to Excel document
     pass
 
-def plot_graphs(): #Inputs from csv
-    pass
 
-def cursors_inputs(): #Cursers to Excel document
-    pass
 def read_exel():
     pass
 
-if __name__ == "__main__":
-    name, current, temp ,Vmax, Vmin, sweepTime, laserControler = sys.argv
 
-    thresholdCurrent = find_threshold_current()
+if __name__ == "__main__":
+    name, current, temp, Vmax, Vmin, sweepTime, laserControler = sys.argv
+
+    # thresholdCurrent = find_threshold_current()
     set_laser_values(current, temp, laserControler)
-    run_labVIEW(Vmax,Vmin,sweepTime)
+    run_labVIEW(Vmax, Vmin, sweepTime)
     read_csv()
     plot_graphs()
     cursors_inputs()
