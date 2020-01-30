665d614866294d35bcaefae2045b39e015ee395f
jchuri <38012139+jchuri@users.noreply.github.com>
2020-01-30 13:22:34 -0800

    Added getLaserStatus and getLaserCurrent

--- Laser_controller_functions.py
+++ Laser_controller_functions.py
@@ -4,65 +4,83 @@ rm = pyvisa.ResourceManager()
 ThorLabs = rm.open_resource("")
 Arroyo = rm.open_resource("")
 
+
 def getID():
     pass
 
+
 def turnOnLaser(LaserController):
-    if LaserController.lower() == 'arroyo':
+    if LaserController.lower() == "arroyo":
         pass
     elif LaserController.lower() == "thor labs":
         pass
-    else: 
+    else:
         print("Laser Controller doesn't exsist")
+
 
 def turnOffLaser(LaserController):
-    if LaserController.lower() == 'arroyo':
+    if LaserController.lower() == "arroyo":
         pass
     elif LaserController.lower() == "thor labs":
         pass
-    else: 
+    else:
         print("Laser Controller doesn't exsist")
 
-def setCurrent(current,LaserController):
-    if LaserController.lower() == 'arroyo':
+
+def setCurrent(current, LaserController):
+    if LaserController.lower() == "arroyo":
         pass
     elif LaserController.lower() == "thor labs":
         pass
-    else: 
+    else:
         print("Laser Controller doesn't exsist")
+
 
 def setTempurature(temperature, LaserController):
-    if LaserController.lower() == 'arroyo':
+    if LaserController.lower() == "arroyo":
         pass
     elif LaserController.lower() == "thor labs":
         pass
-    else: 
+    else:
         print("Laser Controller doesn't exsist")
+
 
 def getCurrent(LaserController):
-    if LaserController.lower() == 'arroyo':
-        pass
+    if LaserController.lower() == "arroyo":
+        current = Arroyo.querry("LASer:LDI?")
+        return current
     elif LaserController.lower() == "thor labs":
-        pass
-    else: 
+        current = ThorLabs.querry(":ILD:ACT?")
+        current = current.split(" ")
+        return current[1]
+    else:
         print("Laser Controller doesn't exsist")
+
 
 def getTemperature(LaserController):
-    if LaserController.lower() == 'arroyo':
+    if LaserController.lower() == "arroyo":
         pass
     elif LaserController.lower() == "thor labs":
         pass
-    else: 
+    else:
         print("Laser Controller doesn't exsist")
+
 
 def getLaserStatus(LaserController):
-    if LaserController.lower() == 'arroyo':
-        pass
+    if LaserController.lower() == "arroyo":
+        response = Arroyo.querry("LASer:OUTput?")
+        if response == 1:
+            return True
+        elif response == 0:
+            return False
+
     elif LaserController.lower() == "thor labs":
-        pass
-    else: 
+        response = ThorLabs.querry(":LASER?")
+        response = response.split(" ")
+        if response[1] == "OFF":
+            return False
+        elif response[1] =="ON":
+            return True
+
+    else:
         print("Laser Controller doesn't exsist")
-
-
-
-
